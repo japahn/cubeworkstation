3030d3d9ea7f11ff1a6dbe0082b7ddd12f8c0e2d
Japahn <japahn@yahoo.com.br>
2016-11-27 16:23:14 -0200

    Making boosters per player.

--- cubeworkstation/run_draft.py
+++ cubeworkstation/run_draft.py
@@ -2,7 +2,7 @@ from the_cube import TheCube
 
 cube = TheCube()
 
-boosters = cube.make_boosters(3)
+boosters = cube.make_boosters_for_players(1)
 for booster in boosters:
     print booster
     print
--- cubeworkstation/the_cube.py
+++ cubeworkstation/the_cube.py
@@ -7,9 +7,10 @@ class TheCube(object):
     def __init__(self):
         self.cube = build_cube()
 
-    def make_boosters(self, num_boosters):
-        main_section = self.cube.sections()['mod_main']
-        drawn_from_main = main_section.draw_cards(num_boosters * 15)
+    def make_boosters_for_players(self, num_players):
+        drawn_from_main = self.cube.sections()['mod_main'].draw_cards(num_players * 45)
+        #drawn_from_lands = self.cube.sections()['mod_lands'].draw_cards(num_players * 4)
+
         cards_grouped_by_booster = grouper(15, drawn_from_main)
 
         return [CardPile('Booster', b) for b in cards_grouped_by_booster]
