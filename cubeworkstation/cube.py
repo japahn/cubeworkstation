4104d253981f180e7fcafdadac5d31f3ca5a78bc
Japahn <japahn@yahoo.com.br>
2016-11-27 16:09:54 -0200

    Reading all cube data.

--- cubeworkstation/cube.py
+++ cubeworkstation/cube.py
@@ -1,6 +1,21 @@
 class Cube(object):
     def __init__(self):
         self._sections = {}
+        self._section_order = []
+        self._card_count = 0
 
     def sections(self):
         return self._sections
+
+    def add_section(self, pile):
+        self._sections[pile.name()] = pile
+        self._section_order.append(pile.name())
+        self._card_count += pile.size()
+
+    def __str__(self):
+        string_repr = '-- Cube (%d) --\n' % self._card_count
+        for section_name in self._section_order:
+            section = self.sections()[section_name]
+            string_repr += '%s (%d)\n' % (section.name(), section.size())
+        string_repr += '-- end --'
+        return string_repr
--- cubeworkstation/cube_data.py
+++ cubeworkstation/cube_data.py
@@ -1,17 +1,26 @@
 from card import Card
 from card_pile import CardPile
+from cube import Cube
 
-def read_text_pool(file_name):
+def read_text_pool(section_name, file_name):
     with file(file_name) as f:
         lines = f.readlines()
     lines = map(lambda s: s.strip(), lines)
     cards = map(lambda s: Card(s), lines)
-    card_pile = CardPile(file_name, cards)
+    card_pile = CardPile(section_name, cards)
     return card_pile
 
-def read_all_mods():
-    mod_lands_pile = read_text_pool('data/mod_lands.txt')
-    print mod_lands_pile
+def build_cube():
+    cube = Cube()
+    cube.add_section(read_text_pool('mod_lands', 'data/mod_lands.txt'))
+    cube.add_section(read_text_pool('mod_main', 'data/mod_main.txt'))
+    cube.add_section(read_text_pool('mod_simple', 'data/mod_simple.txt'))
+    cube.add_section(read_text_pool('module1', 'data/module1.txt'))
+    cube.add_section(read_text_pool('module2', 'data/module2.txt'))
+    cube.add_section(read_text_pool('module3', 'data/module3.txt'))
+    cube.add_section(read_text_pool('module4', 'data/module4.txt'))
+    return cube
 
 if __name__ == '__main__':
-    read_all_mods()
+    cube = build_cube()
+    print cube
