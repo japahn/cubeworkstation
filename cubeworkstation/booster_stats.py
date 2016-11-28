bb28dc2882697d893dcb85f707ae9bd2b1678c2a
Japahn <japahn@yahoo.com.br>
2016-11-28 00:09:17 -0200

    Refactored BoosterStats to a file.

---
+++ cubeworkstation/booster_stats.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+class BoosterStats(object):
+
+    def __init__(self, cube):
+        self._section_names = sorted(cube.sections().keys())
+        self._boosters_seen = 0
+        self._section_distribution = defaultdict(lambda: defaultdict(int))
+
+    def process_booster(self, booster):
+        self._boosters_seen += 1
+        cards_per_section = defaultdict(int)
+        for card in booster.cards():
+            cards_per_section[card.section()] += 1
+
+        for section_name in self._section_names:
+            cards_in_section = cards_per_section[section_name]
+            self._section_distribution[section_name][cards_in_section] += 1
+
+    def output(self):
+        print 'Booster Stats for %d boosters' % self._boosters_seen
+        print
+        for section_name in self._section_names:
+            num_distributions = len(self._section_distribution[section_name])
+            if num_distributions == 0:
+                continue
+
+            if (num_distributions == 1
+                and (0 in self._section_distribution[section_name].keys())):
+                continue
+
+            print 'Section %s:' % section_name
+            max_occurrences = max(self._section_distribution[section_name].keys())
+            for num_occurrences in xrange(max_occurrences + 1):
+                frequency = self._section_distribution[section_name][num_occurrences]
+                percentage = float(frequency) / self._boosters_seen
+                print '    %d occurrences: %d (%s)'\
+                      % (num_occurrences, frequency, '{:.2%}'.format(percentage))
+            print
--- cubeworkstation/run_draft.py
+++ cubeworkstation/run_draft.py
@@ -1,49 +1,12 @@
-from collections import defaultdict
 import webbrowser
 
 from booster_output import output_boosters
+from booster_stats import BoosterStats
 from the_cube import TheCube
-
-class BoosterStats(object):
-
-    def __init__(self, cube):
-        self._section_names = sorted(cube.sections().keys())
-        self._boosters_seen = 0
-        self._section_distribution = defaultdict(lambda: defaultdict(int))
-
-    def process_booster(self, booster):
-        self._boosters_seen += 1
-        cards_per_section = defaultdict(int)
-        for card in booster.cards():
-            cards_per_section[card.section()] += 1
-
-        for section_name in self._section_names:
-            cards_in_section = cards_per_section[section_name]
-            self._section_distribution[section_name][cards_in_section] += 1
-
-    def output(self):
-        print 'Booster Stats for %d boosters' % self._boosters_seen
-        print
-        for section_name in self._section_names:
-            num_distributions = len(self._section_distribution[section_name])
-            if num_distributions == 0:
-                continue
-
-            if (num_distributions == 1
-                and (0 in self._section_distribution[section_name].keys())):
-                continue
-
-            print 'Section %s:' % section_name
-            max_occurrences = max(self._section_distribution[section_name].keys())
-            for num_occurrences in xrange(max_occurrences + 1):
-                frequency = self._section_distribution[section_name][num_occurrences]
-                percentage = float(frequency) / self._boosters_seen
-                print '    %d occurrences: %d (%s)'\
-                      % (num_occurrences, frequency, '{:.2%}'.format(percentage))
-            print
 
 RUNS = 1
 OUTPUT_TO_HTML = True
+HTML_LOCATION ='output/boosters.html'
 
 if __name__ == '__main__':
     the_cube = TheCube()
@@ -59,7 +22,7 @@ if __name__ == '__main__':
             booster_stats.process_booster(b)
 
     if OUTPUT_TO_HTML:
-        output_boosters('output/boosters.html', boosters)
-        webbrowser.open('output/boosters.html')
+        output_boosters(HTML_LOCATION, boosters)
+        webbrowser.open(HTML_LOCATION)
 
     booster_stats.output()
